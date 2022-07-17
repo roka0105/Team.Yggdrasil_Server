344328f3be36d917492597e20a95734039ed9a0f
roka0105 <cocapepsi123@daum.net>
2022-07-18 00:32:05 +0900

    알고리즘 풀기 백준 1918 아이디어 연산자 우선순위에 따라 입력받은 문자열에 ( )  를 삽입한다. 스택에 문자열 뒤에서 부터 넣는다. 스택에서 빼올때 문자는 출력 (를 만났다면 ) 만날때까지 알파벳 출력 연산자는 연산자 저장하는 스택에 쌓기. )만날때마다 연산자 스택에서 하나씩 pop 하기

--- Stl만들기 + 백준/Stl/main.cpp
+++ Stl만들기 + 백준/Stl/main.cpp
@@ -5,25 +5,60 @@
 int main()
 {
 	Stack<char> chs;
+	Stack<char> mark;
 	char str[BUFSIZE];
 	scanf("%s", str);
 	char* ptr = str;
-	while (*ptr)
+	int size = strlen(str);
+	for (int i = size - 1; i >= 0; i--)
 	{
-		switch (*ptr)
+		chs.push(ptr[i]);
+	}
+	char output[BUFSIZE];
+	char* output_ptr = output;
+	int check_count = 0;
+	while (!chs.IsEmpty())
+	{
+
+		switch (chs.peek())
 		{
+		case '+':
+		case '-':
+		case '/':
+		case '*':
+		{	if (check_count > 0)
+			mark.push(*ptr);
+		    else if (*(++ptr) == '(')
+		    {
+			mark.push(*(--ptr));
+		    }
+		    else
+		    {
+		    	char temp = *ptr;
+		    	ptr++;
+		    	*output_ptr = *ptr;
+		    	output_ptr++;
+		    	*output_ptr = temp;
+		    }
+		break;
+		}
 		case '(':
+			check_count++;
 			break;
 		case ')':
+			check_count--;
+			*output_ptr = mark.pop();
+			output_ptr++;
 			break;
-
+		default:
+			*output_ptr = chs.pop();
+			output_ptr++;
+			break;
 		}
-		chs.push(*ptr);
 		ptr++;
 	}
 
-	while (!chs.IsEmpty())
-	{
-		my_printf("%c", chs.pop());
-	}
+
+	my_printf("%s", output_ptr);
+
 }
