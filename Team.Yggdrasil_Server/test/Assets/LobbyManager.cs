c6a38f963edb8b24bdd06666b8d5ab6d4641cef2
DESKTOP-HOA3IRT\pc <cocapepsi123@daum.net>
2022-07-20 13:47:44 +0900

    client update 정리 좀 함. 아직 덜 함.

--- Team.Yggdrasil_Server/test/Assets/LobbyManager.cs
+++ Team.Yggdrasil_Server/test/Assets/LobbyManager.cs
@@ -53,6 +53,9 @@ public class LobbyManager : Singleton_Ver2.Singleton<LobbyManager>
         PageRoom = 2048,
         MAX
     }
+
+    #region server send process
+    //로비 진입에 대한 신호를 서버에 전송
     public void LobbyEnterSendProcess(bool _multi)
     {
         Debug.Log("lobbyenter");
@@ -71,7 +74,8 @@ public class LobbyManager : Singleton_Ver2.Singleton<LobbyManager>
         M_MainTh.SendQueue_Push(M_Packet.PackPacking(protocol));
         MainThread.m_WaitforSendThread.Set();
     }
-    public void CreateRoomSendProcess(string _name,string _pw)
+    //방 생성 요청 신호를 서버에 전송
+    public void CreateRoomSendProcess(string _name, string _pw)
     {
         uint protocol = 0;
 
@@ -81,6 +85,35 @@ public class LobbyManager : Singleton_Ver2.Singleton<LobbyManager>
 
         M_MainTh.SendQueue_Push(M_Packet.PackPacking(protocol, _name, _pw));
         MainThread.m_WaitforSendThread.Set();
+    }
+    #endregion
+
+    #region server recv process
+    //서버에서 받아온 recv packet 을 처리해서 패킷에 대한 결과를 처리한다.
+    private void ResultProcess(uint _protocol, byte[] _recvbuf)
+    {
+        uint detailprotocol = M_Protocol.GetDetailProtocol(_protocol);
+        m_ResultProcess[(DETAILPROCOTOL)detailprotocol]?.Invoke(_recvbuf);
+    }
+    private void LobbyRecvResult(byte[] _recvdata)
+    {
+        Debug.Log("LobbyResult");
+        //unpack
+        //멀티,싱글인지에 따라 처리.
+        //결과체크
+        //결과
+    }
+    private void RoomlistRecvResult(byte[] _recvdata)
+    {
+        Debug.Log("RoomlistResult");
+        //roommanager 호출해서 받아온 방 정보 넘기기.
+    }
+    #endregion
+
+    #region client Input Func
+    public void LobbyEnterBtn(bool _multi)
+    {
+        LobbyEnterSendProcess(_multi);
     }
     public void CreateRoomBtn()
     {
@@ -106,42 +139,29 @@ public class LobbyManager : Singleton_Ver2.Singleton<LobbyManager>
     {
 
     }
-    
-    private void ResultProcess(uint _protocol, byte[] _recvbuf)
-    {
-        uint detailprotocol = M_Protocol.GetDetailProtocol(_protocol);
-        m_ResultProcess[(DETAILPROCOTOL)detailprotocol]?.Invoke(_recvbuf);
-    }
-    private void LobbyResult(byte[] _recvdata)
-    {
-        Debug.Log("LobbyResult");
-        //unpack
-        //멀티,싱글인지에 따라 처리.
-        //결과체크
-        //결과
-    }
-    private void RoomlistResult(byte[] _recvdata)
-    {
-        Debug.Log("RoomlistResult");
-        //roommanager 호출해서 받아온 방 정보 넘기기.
-    }
+    #endregion
+
+    #region client func
     public void Init()
     {
         M_MainTh = MainThread.Instance;
         M_Protocol = ProtocolManager.Instance;
         M_Packet = PacketManager.Instance;
-        m_ResultProcess = new Dictionary<DETAILPROCOTOL, _ResultProcess>();
-        m_ResultProcess.Add(DETAILPROCOTOL.LobbyResult, LobbyResult);
-        m_ResultProcess.Add(DETAILPROCOTOL.RoomlistResult, RoomlistResult);
+
         M_MainTh.RecvProcess_Register((int)MAINPROTOCOL.LOBBY, ResultProcess);
 
-        foreach(LobbyUseWindow type in m_window_type)
+        m_ResultProcess = new Dictionary<DETAILPROCOTOL, _ResultProcess>();
+        m_ResultProcess.Add(DETAILPROCOTOL.LobbyResult, LobbyRecvResult);
+        m_ResultProcess.Add(DETAILPROCOTOL.RoomlistResult, RoomlistRecvResult);
+      
+
+        foreach (LobbyUseWindow type in m_window_type)
         {
             m_Windows.Add(type, m_window_objs[(int)type]);
         }
         ActiveChangeWindow(LobbyUseWindow.None, LobbyUseWindow.CreateRoom);
     }
-
+    //윈도우 창 활성화 비활성화 근데 이거 그냥 windowmanager에 빼놓기.
     private void ActiveChangeWindow(LobbyUseWindow _truewindow, LobbyUseWindow _falsewindow)
     {
         if (_truewindow != LobbyUseWindow.None)
@@ -149,6 +169,9 @@ public class LobbyManager : Singleton_Ver2.Singleton<LobbyManager>
         if (_falsewindow != LobbyUseWindow.None)
             m_Windows[_falsewindow].SetActive(false);
     }
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
