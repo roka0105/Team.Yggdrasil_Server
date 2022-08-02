941875857c79b223ec83caab348eb774ef6abda6
DESKTOP-HOA3IRT\pc <cocapepsi123@daum.net>
2022-08-02 14:20:21 +0900

    server update sector manager 공용 틀

---
+++ Team.Yggdrasil_Server/IOCP_Server/IOCP_Server/CSector.h
@@ -0,0 +1,14 @@
+#pragma once
+class CSector
+{
+
+private:
+	//List<CPlayer> m_playerlist
+	//List<CMonster> m_monsterlist
+	//CBoss*         m_boss
+	//List<CSpirit>   m_spiritlist
+	//List<CObject>   m_objectlist
+	//List<CItem>     m_itemlist
+	//List<CSector>   m_view_sectorlist
+};
+
---
+++ Team.Yggdrasil_Server/IOCP_Server/IOCP_Server/CSectorMgr.h
@@ -0,0 +1,15 @@
+#pragma once
+class CSectorMgr
+{
+public:
+	//플레이어 샌드패킷
+	//void PlayerSendPacket(CSession* ,Protocol,moveflag)
+	//void checksector()
+	//몬스터 샌드패킷
+	//void MonsterSendPacket(CSession*,CMonster*, Protocol,moveflag)
+	//아이템 샌드패킷
+	//void ItemSendPacket(CSession*,Protocol,_onoff_flag)
+private:
+	//List<CSector> m_sectorlist;
+};
+
