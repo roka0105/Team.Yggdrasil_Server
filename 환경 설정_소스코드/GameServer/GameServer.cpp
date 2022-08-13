ae1eaf726b5d408dad93ad40160482825dc65edd
roka0105 <cocapepsi123@daum.net>
2022-08-13 22:52:44 +0900

    인프런 서버 강의 공부 코드

---
+++ 환경 설정_소스코드/GameServer/GameServer.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "CorePch.h"
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <Windows.h>
+#include <future>
+
+queue<int32> q;
+stack<int32> s;
+
+void Push()
+{
+	while (true)
+	{
+		int32 value = rand() % 100;
+		q.push(value);
+
+		this_thread::sleep_for(10ms);
+	}
+}
+void Pop()
+{
+	while (true)
+	{
+		if(q.empty())
+			continue;
+		int32 data = q.front();
+		q.pop();
+		cout << data << endl;
+	}
+}
+int main()
+{
+	thread t1(Push);
+	thread t2(Pop);
+
+	t1.join();
+	t2.join();
+}
